39b5df8769c72bd0681da65c177a9f00aa37d55f
Yong Zhang <yongzhang@weber.edu>
2016-02-11 14:45:16 -0700

    fix the expiration date of the fund types so that they could be loaded to the system.

---
+++ Update20160211.sql
@@ -0,0 +1,3 @@
+UPDATE `wmnsffunds`.`fund_type` SET `effective_end`='2016-12-31 23:59:59' WHERE `id`='1';
+UPDATE `wmnsffunds`.`fund_type` SET `effective_end`='2016-06-30 23:59:59' WHERE `id`='2';
+UPDATE `wmnsffunds`.`fund_type` SET `effective_end`='2016-12-23 00:00:00' WHERE `id`='3';
