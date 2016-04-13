e532a0dbcf62497078900d531392b5898ec84a38
Tatetaylor <tatewtaylor@yahoo.com>
2016-04-12 21:55:04 -0600

    Updates to database especially transactions to more appropriately reflect data to be stored

--- DBDumpTest.sql
+++ DBDumpTest.sql
@@ -8,7 +8,7 @@ USE `wmnsffunds`;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNbalanceECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
@@ -103,8 +103,9 @@ DROP TABLE IF EXISTS `fund_type`;
 CREATE TABLE `fund_type` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `Description` varchar(255) DEFAULT NULL,
-  `effective_start` datetime DEFAULT NULL,
-  `effective_end` datetime DEFAULT NULL,
+  `effective_start` datetime NOT NULL,
+  `effective_end` datetime NOT NULL,
+  `fundTotal` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -115,9 +116,11 @@ CREATE TABLE `fund_type` (
 
 LOCK TABLES `fund_type` WRITE;
 /*!40000 ALTER TABLE `fund_type` DISABLE KEYS */;
-INSERT INTO `fund_type` VALUES (1,'HIDTA15','2015-01-01 00:00:00','2016-12-31 23:59:59'),
-(2,'15N01','2014-07-01 00:00:00','2016-06-30 23:59:59'),
-(3,'test','2015-12-07 00:00:00','2016-12-23 00:00:00');
+INSERT INTO `fund_type` VALUES (1,'HIDTA15','2015-01-01 00:00:00','2016-12-31 23:59:59',0),
+(2,'15N01','2014-07-01 00:00:00','2016-06-30 23:59:59',1000),
+(3,'test','2015-12-07 00:00:00','2016-12-23 00:00:00',500),
+(4,'Expired','2015-12-07 00:00:00','2016-04-11 00:00:00',300);
+
 /*!40000 ALTER TABLE `fund_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,19 +186,19 @@ CREATE TABLE `transfer_transaction` (
 
 LOCK TABLES `transfer_transaction` WRITE;
 /*!40000 ALTER TABLE `transfer_transaction` DISABLE KEYS */;
-INSERT INTO `transfer_transaction` VALUES (1,'2014-05-09','Test Description',2,12,89222.00,2,1,123,'1111','WC-1111','Deposit'),
+INSERT INTO `transfer_transaction` VALUES (1,'2014-05-09','Test Description',NULL,12,89222.00,2,1,123,'1111','WC-1111','Deposit'),
 (2,'2013-05-26','fun things',12,4,3499.00,23,1,234,'1112','WC-1112','Transfer'),
-(3,'2014-04-16','New Transaction',23,24,39888.00,25,2,NULL,'1114','WC-1114','Expenditure'),
-(4,'2014-04-16','New Transaction',2,4,3988.00,12,1,NULL,'1115','WC-1115','Expenditure'),
-(5,'2015-03-24','TestDB',12,23,100.00,12,2,321,'1116','WC-1116','Deposit'),
-(6,'2015-04-14','Test transaction',24,2,150.00,2,2,NULL,'1113','WC-1113','Expenditure'),
-(7,'2015-04-14','Test transaction at office',4,12,200.00,2,2,NULL,'1113','WC-1113','Expenditure'),
-(8,'2015-04-16','testDate',23,24,1.00,2,1,NULL,NULL,NULL,'Expenditure'),
-(9,'2015-04-18','testType 2',2,4,2.00,2,1,NULL,NULL,NULL,'Deposit'),
-(10,'2015-04-18','testtype 3',12,23,15.00,2,2,NULL,NULL,NULL,'Expenditure'),
+(3,'2014-04-16','PI',23,NULL,39888.00,25,2,NULL,'1114','WC-1114','Expenditure'),
+(4,'2014-04-16','PE',2,NULL,3988.00,12,1,NULL,'1115','WC-1115','Expenditure'),
+(5,'2015-03-24','TestDB',NULL,23,100.00,12,2,321,'1116','WC-1116','Deposit'),
+(6,'2015-04-14','PE',24,NULL,150.00,2,2,NULL,'1113','WC-1113','Expenditure'),
+(7,'2015-04-14','PI',4,NULL,200.00,2,2,NULL,'1113','WC-1113','Expenditure'),
+(8,'2015-04-16','PE',23,24,1.00,2,1,NULL,NULL,NULL,'Expenditure'),
+(9,'2015-04-18','Deposit',2,4,2.00,2,1,NULL,NULL,NULL,'Deposit'),
+(10,'2015-04-18','PI',12,23,15.00,2,2,NULL,NULL,NULL,'Expenditure'),
 (11,'2015-06-01','test insert from sql',12,12,100.00,12,2,NULL,NULL,NULL,'Deposit'),
 (12,'2015-06-28','End of month transfer',4,12,100.00,12,1,NULL,NULL,NULL,'Expenditure'),
-(13,'2015-06-30','True end of month, please delete entry on 06/28',4,12,100.00,12,1,NULL,NULL,NULL,'Transfer'),
+(13,'2015-06-30','Me to You',4,12,100.00,12,1,NULL,NULL,NULL,'Transfer'),
 (14,'2015-06-29','testing transaction password',4,12,250.00,12,1,NULL,NULL,NULL,'Transfer'),
 (15,'2015-12-07','test 1',12,23,50.00,23,1,12345,'543210','55','Transfer'),
 (16,'2015-10-19','test 2',NULL,12,55.00,23,1,123456,'6543210','testHibernate','Deposit');
@@ -228,11 +231,11 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('Tate','Taylor',2,'$2a$10$AwgpfisBtTaPGhQA5zA.l.VKTGZdf2hijEGyiypMc5f8laCixhHhK','tatetaylor@mail.com',0,20),
+INSERT INTO `user` VALUES ('Tate','Taylor',2,'$2a$10$AwgpfisBtTaPGhQA5zA.l.VKTGZdf2hijEGyiypMc5f8laCixhHhK','tatetaylor@mail.com',1,20),
 	('Yong','Zhang',4,'$2a$10$lJSRlsBEPyDhSn2dCivsTuLl7CugUPqDhHw0N912OHMHM.jvfdhGS','yongzhang@weber.edu',0,0),
 	('Caleb','New',12,'$2a$10$AwgpfisBtTaPGhQA5zA.l.VKTGZdf2hijEGyiypMc5f8laCixhHhK','calebnew@mail.com',1,5),
 	('Spencer','Ferrin',23,'$2a$10$AwgpfisBtTaPGhQA5zA.l.VKTGZdf2hijEGyiypMc5f8laCixhHhK','ferrinsp@mail.com',1,50),
-	('test','user',24,'$2a$10$AwgpfisBtTaPGhQA5zA.l.VKTGZdf2hijEGyiypMc5f8laCixhHhK','test@user.com',0,0),
+	('test','user',24,'$2a$10$AwgpfisBtTaPGhQA5zA.l.VKTGZdf2hijEGyiypMc5f8laCixhHhK','test@user.com',1,0),
 	('Steven','Hoffman',25,'$2a$10$AwgpfisBtTaPGhQA5zA.l.VKTGZdf2hijEGyiypMc5f8laCixhHhK','stevenhoffman@mail.com',1,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -260,7 +263,7 @@ CREATE TABLE `user_permission` (
 
 LOCK TABLES `user_permission` WRITE;
 /*!40000 ALTER TABLE `user_permission` DISABLE KEYS */;
-INSERT INTO `user_permission` VALUES (12,1),(12,2),(23,1),(2,1),(25,1),(4,2),(24,2);
+INSERT INTO `user_permission` VALUES (12,1),(12,2),(23,2),(2,2),(25,2),(4,2),(24,2);
 /*!40000 ALTER TABLE `user_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
