8ae7f157abe7031267292e6f1e41cc781bef590d
Tatetaylor <tatewtaylor@yahoo.com>
2016-06-05 21:05:10 -0600

    Script for server setup with minimal inserts as possible.

---
+++ CleanServerScript.sql
@@ -0,0 +1,174 @@
+CREATE DATABASE  IF NOT EXISTS `wmnsffunds` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `wmnsffunds`;
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: wmnsffunds
+-- ------------------------------------------------------
+-- Server version	5.6.26-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `fund_type`
+--
+
+DROP TABLE IF EXISTS `fund_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fund_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Description` varchar(255) DEFAULT NULL,
+  `effective_start` datetime DEFAULT NULL,
+  `effective_end` datetime DEFAULT NULL,
+  `unallocted_funds` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB auto_increment=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fund_type`
+--
+
+LOCK TABLES `fund_type` WRITE;
+/*!40000 ALTER TABLE `fund_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fund_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permission`
+--
+
+DROP TABLE IF EXISTS `permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permission`
+--
+
+LOCK TABLES `permission` WRITE;
+/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
+INSERT INTO `permission` VALUES (1,'ADMIN'),(2,'USER');
+/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transfer_transaction`
+--
+
+DROP TABLE IF EXISTS `transfer_transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transfer_transaction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `description` varchar(250),
+  `debit_user_id` int(11) DEFAULT NULL,
+  `credit_user_id` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `operator_user_id` int(11) DEFAULT NULL,
+  `fund_type_id` int(11) DEFAULT NULL,
+  `check_number` int(16) DEFAULT NULL,
+  `case_number` varchar(45) DEFAULT NULL,
+  `ci_number` varchar(45) DEFAULT NULL,
+  `transaction_type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transaction_ibfk_1` (`fund_type_id`),
+  KEY `fk_debit_user` (`debit_user_id`),
+  KEY `fk_credit_user` (`credit_user_id`),
+  KEY `fk_operator_user` (`operator_user_id`),
+  CONSTRAINT `fk_credit_user` FOREIGN KEY (`credit_user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_debit_user` FOREIGN KEY (`debit_user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_operator_user` FOREIGN KEY (`operator_user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `transfer_transaction_ibfk_1` FOREIGN KEY (`fund_type_id`) REFERENCES `fund_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transfer_transaction`
+--
+
+LOCK TABLES `transfer_transaction` WRITE;
+/*!40000 ALTER TABLE `transfer_transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transfer_transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `first_name` varchar(25) DEFAULT NULL,
+  `last_name` varchar(25) DEFAULT NULL,
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `balance` int(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_permission`
+--
+
+DROP TABLE IF EXISTS `user_permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_permission` (
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `permission_id` (`permission_id`),
+  CONSTRAINT `user_permission_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `user_permission_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_permission`
+--
+
+LOCK TABLES `user_permission` WRITE;
+/*!40000 ALTER TABLE `user_permission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_permission` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-05 15:32:47
